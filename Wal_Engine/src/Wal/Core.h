dafbfc6247a015adf750d3c3d6b9c7d72229e710
DinoDionysos <ai21m014@technikum-wien.at>
2022-03-22 14:20:23 +0100

    Add WAL_API macro

--- Wal_Engine/src/Wal/Core.h
+++ Wal_Engine/src/Wal/Core.h
@@ -1,1 +1,13 @@
 #pragma once
+
+// automatically distinguish between ex and import by defining WAL_API
+// depending on if WL_BUILD_DLL is defined in preprocessor definitions
+#ifdef WL_PLATFORM_WINDOWS
+	#ifdef WL_BUILD_DLL
+		#define WAL_API __declspec(dllexport)
+	#else
+		#define WAL_API __declspec(dllimport)
+	#endif
+#else
+	#error Wal only supports Windows!
+#endif
