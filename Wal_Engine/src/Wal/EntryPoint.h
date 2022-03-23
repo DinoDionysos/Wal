ccabc057ef806daf613e94110046972ff61168fb
DinoDionysos <ai21m014@technikum-wien.at>
2022-03-23 23:51:02 +0100

    use the logger macros in EntryPoint.h for testing

--- Wal_Engine/src/Wal/EntryPoint.h
+++ Wal_Engine/src/Wal/EntryPoint.h
@@ -8,6 +8,11 @@ extern Wal::Application* Wal::CreateApplication();
 // externaly in the respective application that wants to access the entry point
 int main(int argc, char** argv)
 {
+	Wal::Log::Init();
+	WL_CORE_WARN("Initialized Log!");
+	int a = 5;
+	WL_CLIENT_INFO("Hello! Var={0}", a);
+
 	auto app = Wal::CreateApplication();
 	app->Run();
 	delete app;
