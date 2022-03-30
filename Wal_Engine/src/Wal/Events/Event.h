75cab500444c5a24dba699d365c3a4002fde33a0
DinoDionysos <ai21m014@technikum-wien.at>
2022-03-30 13:40:04 +0200

    introduce the event system

---
+++ Wal_Engine/src/Wal/Events/Event.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "Wal/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Wal {
+
+	// Events in Wal are currently blocking, meaning when an event occurs it
+	// immediately gets dispatched and must be dealt with right then an there.
+	// For the future, a better strategy might be to buffer events in an event
+	// bus and process them during the "event" part of the update stage.
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		// "bitfield" so multiple categories can be assigned
+		// f.e. 00010001 is MouseButton and Application
+		// #define BIT(x) (1 << x)
+		EventCategoryApplication    = BIT(0),
+		EventCategoryInput          = BIT(1),
+		EventCategoryKeyboard       = BIT(2),
+		EventCategoryMouse          = BIT(3),
+		EventCategoryMouseButton    = BIT(4)
+	};
+
+	// GetStaticType() to get the type of a class (that derives from Event)
+	// GetEventType() to get the type of an instance of a class (that derives from Event)
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+	class WAL_API Event
+	{
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category; //bitwise! 1&1 = 1, Rest 0
+		}
+	protected:
+		// flag if the event is still needed or not
+		bool m_Handled = false;
+	};
+
+	// gets created from an event reference and checks if the event type is
+	// matching the function before running the function
+	class EventDispatcher
+	{
+		// returns bool, takes T&, implemented somewhere else (in the event class f.e.)
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{
+		}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event); // calls the func with the event
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	// for logging, defines << operator
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
+}
+
