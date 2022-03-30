2f687eb47e573e11c72c7fd8c282e67970e80afb
DinoDionysos <ai21m014@technikum-wien.at>
2022-03-30 19:06:47 +0200

    implement the OnWindowClose Event

--- Wal_Engine/src/Wal/Application.cpp
+++ Wal_Engine/src/Wal/Application.cpp
@@ -1,21 +1,38 @@
 #include "wlpch.h"
 #include "Application.h"
 
-#include "Wal/Events/ApplicationEvent.h"
-#include "Wal/Log.h"
 
+#include "Wal/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Wal {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		WL_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 	// dummy function
--- Wal_Engine/src/Wal/Application.h
+++ Wal_Engine/src/Wal/Application.h
@@ -2,10 +2,10 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Wal/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Wal{
-
 
 	class WAL_API Application
 	{
@@ -13,7 +13,9 @@ namespace Wal{
 		Application();
 		virtual ~Application();
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
